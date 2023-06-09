3b3696961f5293e83d03bae004304cb43f265465
Aman-Jangid <aman2003jangid@gmail.com>
2023-06-09 15:22:07 +0530

    create media query for maxWidth 480px

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,15 +4,21 @@ import clickSound from '../assets/select.mp3';
 export default function Card(props) {
   const { name, image } = props.pokemon;
 
+  const mobileWidth = '100px';
+  const styles = {
+    '--mobile-width': props.dimension.width,
+    '--mobile-height': props.dimension.height,
+  };
   const [play] = useSound(clickSound);
 
+  console.log(styles);
   const onClickHandle = (e) => {
     props.scoreSetter(e);
     play();
   };
 
   return (
-    <div className="card" style={props.dimension}>
+    <div className="card" style={styles}>
       <div
         className="overlay"
         id={props.id}
